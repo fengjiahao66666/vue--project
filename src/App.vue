c170502b2f1d556cc6e18f2ad0a1ccaccd8ecf84
fengjiahao66666 <2934182834@qq.com>
2023-03-07 15:14:40 +0800

    merge

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header></Header>
+    <!-- 路由组件出口的地方 -->
+    <router-view></router-view>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//引入
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: '',
+  components:{
+    Header,
+    Footer
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less">
+    .footer {
+        background-color: #eaeaea;
+
+        .footer-container {
+            width: 1200px;
+            margin: 0 auto;
+            padding: 0 15px;
+
+            .footerList {
+                padding: 20px;
+                border-bottom: 1px solid #e4e1e1;
+                border-top: 1px solid #e4e1e1;
+                overflow: hidden;
+                padding-left: 40px;
+
+                .footerItem {
+                    width: 16.6666667%;
+                    float: left;
+
+                    h4 {
+                        font-size: 14px;
+                    }
+
+                    .footerItemCon {
+                        li {
+                            line-height: 18px;
+                        }
+                    }
+
+                    &:last-child img {
+                        width: 121px;
+                    }
+                }
+            }
+
+            .copyright {
+                padding: 20px;
+
+                .helpLink {
+                    text-align: center;
+
+                    li {
+                        display: inline;
+
+                        .space {
+                            border-left: 1px solid #666;
+                            width: 1px;
+                            height: 13px;
+                            background: #666;
+                            margin: 8px 10px;
+                        }
+                    }
+                }
+
+                p {
+                    margin: 10px 0;
+                    text-align: center;
+                }
+            }
+        }
+    }
 </style>
---
+++ src/pages/Search/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>我是搜索
+    <h1>params参数---{{ $route.params.keyword }}---{{ keyword }}</h1>
+    <h1>query参数---{{ $route.query.k }}----{{ k }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'',
+  //路由组件是可以传递props数据
+  props:['keyword','k']
+}
+</script>
+
+<style>
+
+</style>
